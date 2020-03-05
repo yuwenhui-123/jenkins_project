95a103ac064d933424684340f7a1cb702299be43
yuwenhui <603702250@qq.com>
2020-03-05 11:31:04 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,17 @@
+class TestLogin:
+
+    def test_login1(self):
+        print("0")
+        assert 1
+
+    def test_login2(self):
+        print("1")
+        assert 0
+
+    def test_login3(self):
+        print("4")
+        assert 0
+
+    def test_login4(self):
+        print("3")
+        assert 1
